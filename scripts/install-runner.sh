84440694170da4bb1c74fbe2e31309941f302833
iyear <ljyngup@gmail.com>
2025-10-15 02:17:35 +0800

    fix: rm runner original file

--- scripts/install-runner.sh
+++ scripts/install-runner.sh
@@ -5,6 +5,8 @@ VERSION="2.320.0"
 mkdir -p /home/runner/actions-runner
 cd /home/runner/actions-runner || exit
 
-curl -O -L https://github.com/actions/runner/releases/download/v2.320.0/actions-runner-linux-x64-$VERSION.tar.gz
+file="actions-runner-linux-x64-$VERSION.tar.gz"
+
+curl -O -L https://github.com/actions/runner/releases/download/v2.320.0/$file
 # Extract the installer
-tar xzf ./actions-runner-linux-x64-$VERSION.tar.gz
+tar xzf $file && rm -f $file
